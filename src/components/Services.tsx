40f52801a20a29c71d915e30c6f8e96d5f579f55
Victor Manuel <vm74531@gmail.com>
2026-03-11 14:14:57 -0300

    style(Services): Mudandas no grid, titulo

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -93,7 +93,7 @@ export default function Services() {
         <div className="text-center mb-16">
           <h2 className="text-3xl font-bold text-musgo dark:text-white mb-4">MEUS SERVIÇOS</h2>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 font-poppins font-normal">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 font-poppins font-normal">
           {services.map((service, index) => (
             <ServiceCard key={index} service={service} index={index} />
           ))}
